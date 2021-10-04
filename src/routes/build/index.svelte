58d4a03ef8e9821496edf14cf30734f70dd9076b
David Hunt <git@dnotes.net>
2021-10-03 21:48:28 -0700

    Starting builder

---
+++ src/routes/build/index.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+
+  import { page } from '$app/stores'
+  import Game from '$lib/Game'
+  import Pile from '$lib/Stack.svelte'
+  import { game, maxCardWidth } from '$lib/data/stores'
+
+    let config = $page.query.get('g')
+    if (!config) config = 'klondike-vegas'
+
+    let deck = $page.query.get('d') || undefined
+
+    game.set(new Game(config, deck))
+
+    let w=0,h=0
+
+    $: maxCardWidth.set((w / $game.longestRow) - 20)
+    $: rowHeight = $maxCardWidth * 1.5
+
+  </script>
+
+  <div id="game" class="flex flex-col text-white" bind:clientWidth={w} bind:clientHeight={h}>
+    <div id="actions" class="flex-shrink">
+      <button on:click={() => {$game.doUndo();game.set($game)}}>Undo</button>
+      <button on:click={() => {$game.doRedo();game.set($game)}}>Redo</button>
+      <button on:click={() => {game.set(new Game(config))}}>New</button>
+    </div>
+
+    <!-- ROWS -->
+    {#each $game.layout as row, rowIndex}
+    <div class="flex flex-row w-full relative p-4 pointer-events-none" class:justify-center={$game.conf.centerRows} style="height:{$game.conf.overlayRows && (rowIndex !== $game.layout.length - 1) ? $maxCardWidth * .7 : rowHeight + row.padBottom}px;">
+      <!-- PILES -->
+      {#each row.stacks as stack}
+        <div>
+          <Pile {stack} />
+        </div>
+      {/each}
+    </div>
+    {/each}
+
+    {#each $game.footer as row, rowIndex}
+    <div class="flex flex-row w-full relative p-4 pointer-events-none" class:justify-center={$game.conf.centerRows} style="height:{$game.conf.overlayRows && (rowIndex !== $game.footer.length - 1) ? $maxCardWidth * .7 : rowHeight + row.padBottom}px;">
+      {#each row.stacks as stack}
+        <div>
+          <Pile {stack} />
+        </div>
+      {/each}
+    </div>
+    {/each}
+
+    <div class="absolute bottom-0 pl-4"><a href='/play?g={$game.conf.toString()}'>{@html $game.conf.toString().replace(/!/g, '<br>$&').replace(/\|/g, '<br> &nbsp; |')}</a></div>
+  </div>
+
+
