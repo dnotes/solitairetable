926e797a3cc693d72ff06ba8b9ab92b609fb3d29
David Hunt <git@dnotes.net>
2021-10-10 12:28:34 -0700

    improve menu ux

--- src/lib/GameLinks.svelte
+++ src/lib/GameLinks.svelte
@@ -1,10 +1,12 @@
 <script>
+  import { onMount } from 'svelte'
   export let family = ''
 
   import games from '$lib/data/games'
   import GameLink from './GameLink.svelte'
 
   let open = false
+  let menu
   let links = family ? [games[family], ...games[family].variants] : Object.keys(games).map(k => games[k]).filter(g => g.family === g.name)
   let title = family ? `${links[0].title} Variants` : 'All Games'
 
@@ -14,9 +16,33 @@
   let childClass = ''
   export { cls as class, childClass }
 
+  onMount(() => {
+    const handleOutsideClick = (event) => {
+      if (open && !menu.contains(event.target)) {
+        open = false;
+      }
+    };
+
+    const handleEscape = (event) => {
+      if (open && event.key === 'Escape') {
+        open = false;
+      }
+    };
+
+    // add events when element is added to the DOM
+    document.addEventListener('click', handleOutsideClick, false);
+    document.addEventListener('keyup', handleEscape, false);
+
+    // remove events when element is removed from the DOM
+    return () => {
+      document.removeEventListener('click', handleOutsideClick, false);
+      document.removeEventListener('keyup', handleEscape, false);
+    };
+  });
+
 </script>
 
-<div class="relative inline-block px-4 cursor-pointer {cls}">
+<div class="relative inline-block px-4 cursor-pointer {cls}" bind:this={menu} >
   <a class="px-3 block {cls}" href="/" on:click|preventDefault={() => {open=!open}}>{title}</a>
   {#if open}
     <div class="absolute {labelSide}-full min-w-full {cls}" on:click={() => {open=false}}>
