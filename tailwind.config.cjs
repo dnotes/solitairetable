4dc12172eaabec5c036f0c83557619df8eda4813
David Hunt <git@dnotes.net>
2021-10-10 13:42:50 -0700

    safelist styles for dropdown menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,20 @@
 const config = {
 	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
+	purge: {
+		content: ["./src/**/*.{html,js,svelte,ts}",],
+		safelist: [
+			'bottom-full',
+			'top-full',
+			'right-full',
+			'left-full',
+		],
+	},
 	theme: {
 		boxShadow: {
 			DEFAULT: '0 0 4px 1px rgba(0,0,0,.1)'
 		},
 		extend: {}
 	},
-	plugins: [
-		require('@tailwindcss/aspect-ratio'),
-	],
 };
 
 module.exports = config;
