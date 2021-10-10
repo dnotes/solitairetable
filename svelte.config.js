6bf83140970c2296f6e0fd3db1e0515ff2d5e914
David Hunt <git@dnotes.net>
2021-10-10 13:44:07 -0700

    add markdown parsing with vite-plugin-markdown

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,25 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static"
+import { plugin as mdPlugin } from "vite-plugin-markdown"
+import mdAttrs from "markdown-it-attrs"
+import mdit from "markdown-it"
+
+const markdownIt = mdit({
+    xhtmlOut:true,
+    typographer:true,
+}).use(mdAttrs)
+const md = mdPlugin({mode:'html', markdownIt})
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+        vite: {
+            plugins: [
+                md,
+            ]
+        },
         adapter: adapter()
 	},
 
