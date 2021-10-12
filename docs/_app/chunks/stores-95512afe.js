adc3c92e3310c523a1d70ef91766f97cfdddd2aa
David Hunt <git@dnotes.net>
2021-10-12 04:08:05 -0700

    add docs directory for site

---
+++ docs/_app/chunks/stores-95512afe.js
@@ -0,0 +1,1 @@
+import{C as a,P as s}from"./vendor-bbe47adb.js";import{G as e}from"./Game-c6420ef6.js";const o=a(new e),m=a(120),r=s(m,(a=>.14*a)),t=s(m,(a=>.24*a)),b=a({});export{r as a,b as d,t as e,o as g,m};
