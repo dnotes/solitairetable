01127668b3b64faea406d077d044fd77ef546428
David Hunt <git@dnotes.net>
2022-08-28 01:32:23 -0700

    fixes service worker install

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,7 +2,13 @@
 import { build, files } from '$service-worker'
 import { precacheAndRoute } from 'workbox-precaching'
 
-precacheAndRoute([...build, ...files])
+let cacheEntries = [
+  ...build,
+  // for some reason, npm build script generates phantom .DS_Store
+  // @ts-ignore
+  ...files.filter(v => !/(?:\.DS_Store|\.nojekyll$)/.test(v.url))
+]
+precacheAndRoute(cacheEntries)
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
