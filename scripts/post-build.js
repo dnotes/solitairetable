6d880191eedcf2d45b7ff1bc8d9681f41e6e03b0
David Hunt <git@dnotes.net>
2022-09-16 01:55:46 -0700

    change file paths for post-build

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import fs from 'fs'
-let sw = fs.readFileSync('./docs/service-worker.js', 'utf-8')
-fs.writeFileSync('./docs/service-worker.js', sw.replace(/process\.env\.NODE_ENV/g, '"production"'))
+let sw = fs.readFileSync('./.svelte-kit/cloudflare/service-worker.js', 'utf-8')
+fs.writeFileSync('./.svelte-kit/cloudflare/service-worker.js', sw.replace(/process\.env\.NODE_ENV/g, '"production"'))
