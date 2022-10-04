b06f2bbdfa43ffe4a3be587ffa35fc2e25ea2c7e
David Hunt <git@dnotes.net>
2022-10-04 08:35:56 -0700

    retry the service-worker post-build script

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import fs from 'fs'
-let sw = fs.readFileSync('./.svelte-kit/cloudflare/service-worker.js', 'utf-8')
-fs.writeFileSync('./.svelte-kit/cloudflare/service-worker.js', sw.replace(/process\.env\.NODE_ENV/g, '"production"'))
+let sw = fs.readFileSync('./.svelte-kit/output/client/service-worker.js', 'utf-8')
+fs.writeFileSync('./.svelte-kit/output/client/service-worker.js', sw.replace(/process\.env\.NODE_ENV/g, '"production"'))
