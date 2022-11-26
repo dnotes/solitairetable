0387e113f3ff58795e512aa407189e93822a5017
David Hunt <git@dnotes.net>
2022-11-26 14:22:49 +0100

    clean up typescript in utli.ts

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -3,13 +3,13 @@ const digitsStr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy
               //   0       8       16      24      32      40      48      56     63
 
 const digits = digitsStr.split('');
-const digitsMap = {};
+const digitsMap:{[key:string]:number} = {};
 for (var i = 0; i < digits.length; i++) {
     digitsMap[digits[i]] = i;
 }
 
 export const b64 = {
-  encode: function(...int32s) {
+  encode: function(...int32s:number[]) {
     return int32s.map(int32 => {
       var result = '';
       while (true) {
@@ -21,7 +21,7 @@ export const b64 = {
       return result;
     }).join('')
   },
-  decode: function(digitsStr) {
+  decode: function(digitsStr:string):number {
     var result = 0;
     var digits = digitsStr.split('');
     for (var i = 0; i < digits.length; i++) {
@@ -32,11 +32,11 @@ export const b64 = {
 }
 
 export const confString = {
-  encode: function(value, possible) {
-    let bin = possible.replace(new RegExp(`[${value.replace(/[^a-zA-Z0-9]/g,'')}]`, 'gi'), '1').replace(/[^1]/g, 0)
+  encode: function(value:string, possible:string):string {
+    let bin = possible.replace(new RegExp(`[${value.replace(/[^a-zA-Z0-9]/g,'')}]`, 'gi'), '1').replace(/[^1]/g, '0')
     return b64.encode(parseInt(bin,2)).padStart(b64.encode(2^possible.length).length, '0')
   },
-  decode: function(value, possible) {
+  decode: function(value:string, possible:string):string {
     let log = `string: "${value}" | ${possible} = `
     let val = b64.decode(value)
       .toString(2)
@@ -52,10 +52,10 @@ export const confString = {
 }
 
 export const confBoolean = {
-  encode: function(...values) {
+  encode: function(...values:boolean[]):string {
     return b64.encode(parseInt(values.reverse().map(v => v ? 1 : 0).join(''), 2))
   },
-  decode: function(number) {
+  decode: function(number:string):boolean[] {
     let log = `boolean: "${number}" = `
     let val = b64.decode(number).toString(2).split('').reverse().map(v => (v === '1')  ? true : false)
     // console.log(`${log}"${val}"`)
