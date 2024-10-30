4c923d10d5e800334772d151f555bc48bf565a38
David Hunt <git@dnotes.net>
2024-10-31 05:22:30 +1300

    test: added the test for playing a card

--- tests/main.steps.ts
+++ tests/main.steps.ts
@@ -3,6 +3,7 @@ import type { PlaywrightWorld } from '@quickpickle/playwright/PlaywrightWorld'
 import '@quickpickle/playwright/actions'
 import '@quickpickle/playwright/outcomes'
 import '@quickpickle/playwright/world'
+import { expect } from '@playwright/test'
 
 let deals = [
   'OwkrbDZdvVUcTspxHLEgmXNiyGulAfoIWMKFneaBSJPjRhqYCQtz'
@@ -11,7 +12,7 @@ let deals = [
 async function gotoGame(world:PlaywrightWorld, game:string, deal?:string) {
   if (!deal) deal = deals[0]
   await world.page.goto(`http://${world.baseUrl.host}/play/${game}/${deal}`)
-  await world.page.waitForTimeout(200)
+  await world.page.waitForTimeout(800)
 }
 
 Given('I am playing', async (world:PlaywrightWorld) => {
@@ -40,4 +41,12 @@ Given('I have just won a game', async (world:PlaywrightWorld) => {
   await gotoGame(world, 'freecell', 'MmZzFfSslLyYeErRKkXxDdQqjJwWcCpPIiVvBbOohHuUaAnNGgTt')
   await world.page.getByRole('button', { name:'autoplay' }).click({ timeout:500 })
   await world.page.waitForTimeout(1500)
+})
+
+When(/I (?:click|play) the ([A23456789TJQK])([CHDS♣♥♦♠])/i, async (world:PlaywrightWorld, rank:string, suit:string) => {
+  suit = suit.replace('C', '♣').replace('H', '♥').replace('D', '♦').replace('S', '♠')
+  let locator = await world.page.getByRole('img', { name:`${rank}${suit}` })
+  await expect(await locator.count()).toBeGreaterThan(0)
+  let parent = await locator.first().locator('../../..')
+  await parent.click()
 })
